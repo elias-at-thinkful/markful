5485cc4f4d409cca808ab25ea688c9f7a4186543
Rich Greenhill <rgnet1@yahoo.com>
2018-01-24 21:08:22 -0800

    mock data func

--- scripts/index.js
+++ scripts/index.js
@@ -2,32 +2,36 @@
 
 $(() => {
   app.bindEventListeners();
+  debug_mockData();
   app.render();
 });
 
-const mockData = [
-  {
-    title: 'Something',
-    url: 'http://example.com',
-    rating: 4,
-    desc: 'Lorem ipsum etc',
-    id: 1,
-  },
-  {
-    title: 'Article on Cats',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium omnis reprehenderit beatae soluta sapiente optio repellat nulla ipsam, architecto harum in consectetur suscipit unde delectus atque. Ex a soluta eveniet?',
-    url: 'http://google.com',
-    rating: 3,
-    id: 2,
-  },
-  {
-    title: "Naughty Bookmark I shouldn't bookmark",
-    url: 'http://bookmark.com',
-    rating: 5,
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium omnis reprehenderit beatae soluta sapiente optio repellat nulla ipsam, architecto harum in consectetur suscipit unde delectus atque. Ex a soluta eveniet?',
-    id: 3,
-  },
-];
 
-mockData.forEach(b => store.addBookmark(b));
-console.log(store);
+// DEBUG
+const debug_mockData = function() {
+  const mockData = [
+    {
+      title: 'Something',
+      url: 'http://example.com',
+      rating: 2,
+      desc: 'Lorem ipsum etc',
+      id: 1,
+    },
+    {
+      title: 'Article on Cats',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium omnis reprehenderit beatae soluta sapiente optio repellat nulla ipsam, architecto harum in consectetur suscipit unde delectus atque. Ex a soluta eveniet?',
+      url: 'http://google.com',
+      rating: 3,
+      id: 2,
+    },
+    {
+      title: "Naughty Bookmark I shouldn't bookmark",
+      url: 'http://bookmark.com',
+      rating: 5,
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium omnis reprehenderit beatae soluta sapiente optio repellat nulla ipsam, architecto harum in consectetur suscipit unde delectus atque. Ex a soluta eveniet?',
+      id: 3,
+    },
+  ];
+  mockData.forEach(b => store.addBookmark(b));
+};
+
